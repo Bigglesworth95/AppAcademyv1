b4e4f30d7b2bab213606c769792f12101a9341a1
Bigglesworth95 <71407140+Bigglesworth95@users.noreply.github.com>
2021-12-31 13:48:56 -0500

    Add files via upload
    The spec files were provided. The task was to run the lib files, interpret the spec files, and then make the designated changes to eliminate all errors .

---
+++ part_1.rb
@@ -0,0 +1,33 @@
+def average(num1, num2)
+  (num1.to_f+num2.to_f)/2.0
+end
+
+def average_array(arr)
+  arr.sum.to_f/arr.length.to_f
+end
+
+def repeat(str, num)
+  str*num
+end
+
+def yell(str)
+  str = str.upcase!
+  str+"!"
+end
+
+
+def alternating_case(str)
+  str = str.split(" ")
+  str.each_with_index do |let, i|
+    if i == 0
+      str[i].upcase!
+    else
+      if i %2 == 0
+        str[i].upcase!
+      else
+        str[i].downcase!
+      end
+    end
+  end
+  str.join(" ")
+end
---
+++ part_2.rb
@@ -0,0 +1,32 @@
+def hipsterfy(str)
+  vowels = 'aeiou'
+  i = str.length() -1
+  while i > 0
+    if vowels.include?(str[i])
+      return str[0...i]+str[i+1..-1]
+    end
+    i-=1
+  end
+  str
+end
+
+def vowel_counts(str)
+  hash = Hash.new(0)
+  vowels = 'aeiou'
+  str.downcase!
+  str.each_char {|let| hash[let]+=1 if vowels.include?(let) }
+  hash
+end
+
+def caesar_cipher(str, num)
+  alpha = 'abcdefghijklmnopqrstuvwxyz'
+  str.each_char.with_index do |let, i|
+    if alpha.include?(let)
+      index = alpha.index(let)
+      shift = (num+index)%alpha.length
+      str[i] = alpha[shift]
+    end
+  end
+  str
+end
+puts caesar_cipher('aaa1', 1)
