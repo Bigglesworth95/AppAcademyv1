80d1abf5d31eca01534e5150636b049d8671da2c
Bigglesworth95 <71407140+Bigglesworth95@users.noreply.github.com>
2022-01-01 13:22:35 -0500

    Add files via upload

---
+++ part_1.rb
@@ -0,0 +1,56 @@
+def partition(arr, num)
+  less = []
+  more = []
+  arr.each do |i|
+    if i >= num
+      more << i
+    else
+      less << i
+    end
+  end
+  [less, more]
+end
+
+def merge (hash1, hash2)
+  newHash = {}
+  hash2.each do |key, value|
+    newHash[key] = value
+  end
+  hash1.each do |key, value|
+    if newHash.key?(key)
+    else
+      newHash[key] = value
+    end
+  end
+  newHash
+end
+def censor(sent, arr)
+  sent = sent.split(" ")
+  sent.map do |word|
+    if arr.include?(word.downcase)
+      star_vowels(word)
+    end
+  end
+  sent.join(" ")
+end
+
+def star_vowels(word)
+  vowels = 'aeiouAEIOU'
+  word.each_char.with_index do |let, i|
+    if vowels.include?(let)
+      word[i] = "*"
+    end
+  end
+  word
+end
+
+def power_of_two?(num)
+  while num.to_f >1
+    num = num/2.0
+  end
+  if num == 1
+    true
+  else
+    false
+  end
+end
---
+++ part_2.rb
@@ -0,0 +1,33 @@
+def palindrome?(str)
+  newString = ""
+  length = (str.length() -1)
+  while length>=0
+    newString += str[length]
+    length-=1
+  end
+  if str == newString
+    true
+  else
+    false
+  end
+end
+
+def substrings(str)
+subs = []
+(0...str.length).each do |i|
+  (i...str.length).each do |n|
+    subs << str[i..n]
+  end
+end
+subs
+end
+
+def palindrome_substrings(str)
+  ps = []
+  substrings(str).each do |word|
+    if palindrome?(word) && word.length > 1
+      ps << word
+    end
+  end
+  ps
+end
