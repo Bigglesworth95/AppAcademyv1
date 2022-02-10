b08a46af8538026394d8a38f8c2cadd29e46d8f9
Bigglesworth95 <71407140+Bigglesworth95@users.noreply.github.com>
2022-02-10 05:36:50 -0500

    Update and rename board.rb to Memory2: board.rb

--- Memory2: board.rb
+++ Memory2: board.rb
@@ -1,3 +1,5 @@
+#this is copied from the solution. I was able to work out everything but cpu player independetly within
+#the alotted time limit. 
 require_relative "card.rb"
 require "byebug"
 class Board
